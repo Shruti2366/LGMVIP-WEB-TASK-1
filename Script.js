ee8340ffa39d53279f9b71f09321f98e2661c9c6
Shruti2366 <116244803+Shruti2366@users.noreply.github.com>
2023-07-15 14:05:38 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById("input-box");
+const list = document.getElementById("to-do-list");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Enter your Task");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+list.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        save();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        save();
+    }
+}, false);
+
+function save(){
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function show(){
+    list.innerHTML = localStorage.getItem("data");
+}
+
+show();
